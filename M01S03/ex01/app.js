995d81365ad767481464111b8207ec07d2647921
Crina <crina.ana.muntean@gmail.com>
2022-05-24 22:04:51 +0300

    S03 ex01-05

--- M01S03/ex01/app.js
+++ M01S03/ex01/app.js
@@ -3,3 +3,24 @@ var object = {
   surname: 'Muntean',
   age: 28,
 };
+
+console.log(object.name);
+//  === '28'
+console.log(object.age.toString());
+
+// Afiseaza fraza “Ma numesc xxx yyy si am xx ani.” in consola folosind operatorul de concatenare
+console.log(
+  'Ma numesc ' +
+    object.name +
+    ' ' +
+    object.surname +
+    ' si am ' +
+    object.age +
+    ' ani.',
+);
+
+console.warn(
+  `Afiseaza anul in care te-ai nascut scazand varsta pe care ai
+trecut-o in obiect din anul curent (un proces numit hardcodare)`,
+);
+console.log((2022 - object.age).toString());
---
+++ M01S03/ex05/app.js
@@ -0,0 +1,24 @@
+var person = {
+  name: 'Crina',
+  surname: 'Muntean',
+  age: 28,
+  petOwner: false,
+  skills: ['html', 'javascript', 'css'],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larryson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
