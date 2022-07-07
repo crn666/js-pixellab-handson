2902426a9d49fb503fd952a8abc7a7c73c956343
Crina <crina.ana.muntean@gmail.com>
2022-07-07 21:54:56 +0300

    S03 ex 01+03

--- M02S03/ex03/app.js
+++ M02S03/ex03/app.js
@@ -1,1 +1,30 @@
+const stage = document.querySelector('.stage');
 
+stage.addEventListener('mouseover', function () {
+  // "this este elementul de DOM"
+  const message = 'Mouseul este pe scena';
+
+  // console.log(message);
+  showMessage('.message', message);
+});
+
+stage.addEventListener('mouseout', function () {
+  const message = 'Mouseul NU este pe scena';
+
+  showMessage('.message', message);
+});
+
+const showMessage = (targetSelector = '.message', message) => {
+  let element = document.querySelector(targetSelector);
+
+  if (element === null) {
+    // jQuery avea .text()....
+    element = document.createElement('p');
+    element.classList.add(targetSelector.replace('.', ''));
+    document.body.append(element);
+  }
+
+  element.innerText = message;
+};
+
+// fctiile cu arrow nu sunt hoisted
