1a8907dda4726d4da7abae1bb8122cdaacc0fcd5
Crina <crina.ana.muntean@gmail.com>
2022-07-19 19:53:58 +0300

    S04 ex03

--- M02S04/ex03/app.js
+++ M02S04/ex03/app.js
@@ -2,6 +2,7 @@ const controls = document.querySelector('.controls');
 const heroElement = document.querySelector('.hero');
 const stageElement = document.querySelector('.stage');
 const step = 30;
+// maps
 const axis = {
   N: 'y',
   S: 'y',
@@ -13,6 +14,12 @@ const direction = {
   S: 'fw',
   W: 'back',
   E: 'fw',
+  ArrowDown: 'fw',
+  ArrowUp: 'back',
+};
+const arrowMap = {
+  ArrowDown: 'y',
+  ArrowUp: 'y',
 };
 const hero = {
   x: 0,
@@ -34,6 +41,22 @@ controls.addEventListener('click', function (event) {
   const value = target.className;
   const currentAxis = axis[value];
   const currentDirection = direction[value];
+
+  updateHeroPosition(currentAxis, currentDirection);
+
+  renderHero(hero);
+});
+
+document.addEventListener('keydown', (event) => {
+  const arrowPressed = event.code;
+
+  // early return
+  if (!arrowPressed.startsWith('Arrow')) {
+    return;
+  }
+
+  const currentAxis = arrowMap[arrowPressed];
+  const currentDirection = direction[arrowPressed];
 
   updateHeroPosition(currentAxis, currentDirection);
 
